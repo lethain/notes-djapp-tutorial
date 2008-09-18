bf4cf5f7c8b59b590c18be3efcbcff3f345cd93c
Will Larson <lethain@gmail.com>
2008-09-18 09:15:36 -0400

    Created the notes/urls.py file using generic views for display.

---
+++ notes/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from models import Note
+
+
+notes = Note.objects.all()
+
+urlpatterns = patterns(
+    '',
+    (r'^$',
+     'django.views.generic.list_detail.object_list',
+     dict(queryset=notes)),
+    (r'^note/(?P<slug>[-\w]+)/$', 
+     'django.views.generic.list_detail.object_detail',
+     dict(queryset=notes, slug_field='slug')),
+)
+
