1d801a0e8af82f7996bc6791daeb0a34d2aceb33
Will Larson <lethain@gmail.com>
2008-09-18 10:17:15 -0400

    Updated project.urls to serve static media.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,6 +7,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
     (r'^', include('ajax_tut.notes.urls')),
 
     # Uncomment the next line to enable admin documentation:
