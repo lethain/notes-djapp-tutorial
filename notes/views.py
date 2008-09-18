6f987a3b09294e4c58ac3cbefbc57dde79f49506
Will Larson <lethain@gmail.com>
2008-09-18 10:11:32 -0400

    Created the update_note func.

--- notes/views.py
+++ notes/views.py
@@ -12,3 +12,23 @@ def create_note(request):
             return HttpResponseRedirect(new_note.get_absolute_url())
         error_msg = u"Insufficient POST data (need 'slug' and 'title'!)"
     return HttpResponseServerError(error_msg)
+
+def update_note(request, slug):
+    if request.method == "POST":
+        post = request.POST.copy()
+        note = Note.objects.get(slug=slug)
+        if post.has_key('slug'):
+            slug_str = post['slug']
+            if note.slug != slug_str:
+                if Note.objects.filter(slug=slug_str).count() > 0:
+                    error_msg = u"Slug already taken."
+                    return HttpResponseServerError(error_msg)
+                note.slug = slug_str
+        if post.has_key('title'):
+            note.title = post['title']
+        if post.has_key('text'):
+            note.text = post['text']
+        note.save()
+        return HttpResponseRedirect(note.get_absolute_url())
+    error_msg = u"No POST data sent."
+    return HttpResponseServerError(error_msg)
