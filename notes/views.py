c75d9471b4ee43081b9b31d0942f57d57d7e9c70
Will Larson <lethain@gmail.com>
2008-09-18 09:49:34 -0400

    Wrote create_note method.

--- notes/views.py
+++ notes/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from models import Note
+from django.http import HttpResponseRedirect, HttpResponseServerError
+
+def create_note(request):
+    error_msg = u"No POST data sent."
+    if request.method == "POST":
+        post = request.POST.copy()
+        if post.has_key('slug') and post.has_key('title'):
+            title = post['title']
+            slug = post['slug']
+            new_note = Note.objects.create(title=title,slug=slug)
+            return HttpResponseRedirect(new_note.get_absolute_url())
+        error_msg = u"Insufficient POST data (need 'slug' and 'title'!)"
+    return HttpResponseServerError(error_msg)
