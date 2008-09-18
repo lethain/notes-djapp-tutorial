1251aa50795dbacdb5b15f8078a61fd59f43f4be
Will Larson <lethain@gmail.com>
2008-09-18 10:02:16 -0400

    Added note/<slug>/update/ url for updating notes.

--- notes/urls.py
+++ notes/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns(
      'django.views.generic.list_detail.object_detail',
      dict(queryset=notes, slug_field='slug')),
     (r'^create/$','notes.views.create_note'),
+    (r'^note/(?P<slug>[-\w]+)/update/$','notes.views.update_note'),
     
 )
 
