2d43d46cbd61c38a6a87f1b309f1e1213a7016fb
Will Larson <lethain@gmail.com>
2008-09-18 09:05:05 -0400

    Implemented Note model.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    text = models.TextField(blank=True,null=True)
+
+    def __unicode__(self):
+        return u"Note(%s,%s)" % (self.title, self.slug)
+
+    def get_absolute_url(self):
+        return u"/note/%s/" % self.slug
