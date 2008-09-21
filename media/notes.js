87bac7cd37904e74d9603997df0f0445a91600c0
Will Larson <lethain@gmail.com>
2008-09-21 10:43:36 -0400

    Added display_success method to notes.js

--- media/notes.js
+++ media/notes.js
@@ -3,3 +3,8 @@ var display_error = function(msg, elem) {
   var msg_div = $('<div class="error_msg"><p>'+msg+'</p></div>');
   msg_div.insertAfter(elem).fadeIn('slow').animate({opacity: 1.0}, 5000).fadeOut('slow',function() { msg_div.remove(); });
 }; 
+
+var display_success = function(msg, elem) {
+  var msg_div = $('<div class="success_msg"><p>'+msg+'</p></div>');
+  msg_div.insertAfter(elem).fadeIn('slow').animate({opacity: 1.0}, 5000).fadeOut('slow',function() { msg_div.remove(); });
+}; 
