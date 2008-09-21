709bcfc0897c74e0f7f792add742e7ec62aba87e
Will Larson <lethain@gmail.com>
2008-09-20 22:02:36 -0400

    Added notes.js

---
+++ media/notes.js
@@ -0,0 +1,5 @@
+
+var display_error = function(msg, elem) {
+  var msg_div = $('<div class="error_msg"><p>'+msg+'</p></div>');
+  msg_div.insertAfter(elem).fadeIn('slow').animate({opacity: 1.0}, 5000).fadeOut('slow',function() { msg_div.remove(); });
+}; 
