84d1c14de8ddb27c57d8a483f1d0c32aa54ce458
Will Larson <lethain@gmail.com>
2008-09-22 20:44:08 -0400

    Updated urls with ajax custom views.

--- notes/urls.py
+++ notes/urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns(
      'django.views.generic.list_detail.object_detail',
      dict(queryset=notes, slug_field='slug')),
     (r'^create/$','notes.views.create_note'),
+    (r'^ajax_create/$','notes.views.ajax_create_note'),
     (r'^note/(?P<slug>[-\w]+)/update/$','notes.views.update_note'),
-    
+    (r'^note/(?P<slug>[-\w]+)/ajax_update/$','notes.views.ajax_update_note'),
 )
 
